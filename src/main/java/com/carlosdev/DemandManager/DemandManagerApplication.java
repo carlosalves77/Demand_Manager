1a17907d427ee8deffddda004a3dcb6f806896d0
Carlos Alves <carlosaalves07@outlook.com>
2025-10-14 14:09:02 -0300

    feat: commit inicial

---
+++ src/main/java/com/carlosdev/DemandManager/DemandManagerApplication.java
@@ -0,0 +1,13 @@
+package com.carlosdev.DemandManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosdev/DemandManager/DemandManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosdev.DemandManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
