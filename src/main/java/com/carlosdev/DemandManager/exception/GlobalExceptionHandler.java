104e63ab1bc65af5167e6b5ef698c5af781fb2fd
Carlos Alves <carlosaalves07@outlook.com>
2025-10-22 22:07:00 -0300

    feat: foi criado uma verificação request para caso o id passado não seja invilado.

--- src/main/java/com/carlosdev/DemandManager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/carlosdev/DemandManager/exception/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.carlosdev.DemandManager.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,5 +33,23 @@ public class GlobalExceptionHandler {
 
         erros.put("message", ex.getMessage());
         return ResponseEntity.badRequest().body(erros);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadable
+            (HttpMessageNotReadableException ex) {
+
+        Map<String, String> erros = new HashMap<>();
+
+        Throwable cause = ex.getCause();
+
+        if (cause instanceof InvalidFormatException ifx && ifx.getTargetType().equals(UUID.class)) {
+            erros.put("message", "Formato de UUID invalido");
+        } else {
+            erros.put("message", "corpo da requisição está malformado ou ilegível");
+        }
+
+        return new ResponseEntity<>(erros, HttpStatus.BAD_REQUEST);
+
     }
 }
