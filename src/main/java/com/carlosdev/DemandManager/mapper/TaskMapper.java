65363e8337a76b30e862e36ca5bc0ad3ca0b57fc
Carlos Alves <carlosaalves07@outlook.com>
2025-10-22 22:10:21 -0300

    feat: criação do mapper para request e response do task

---
+++ src/main/java/com/carlosdev/DemandManager/mapper/TaskMapper.java
@@ -0,0 +1,49 @@
+package com.carlosdev.DemandManager.mapper;
+
+import com.carlosdev.DemandManager.dto.request.TaskRequestDTO;
+import com.carlosdev.DemandManager.dto.response.TaskResponseDTO;
+import com.carlosdev.DemandManager.model.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class TaskMapper {
+
+
+    public static TaskResponseDTO toResponse(Task task) {
+        if (task == null) {
+            return null;
+        }
+
+        return new TaskResponseDTO(
+                task.getId().toString(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getStatus(),
+                task.getPriority(),
+                task.getDueDate()
+        );
+
+    }
+
+    public static Task toEntity(TaskRequestDTO taskRequestDTO) {
+        if (taskRequestDTO == null) {
+            return null;
+        }
+
+
+        Task taskEntity = new Task();
+        taskEntity.setTitle(taskRequestDTO.title());
+        taskEntity.setDescription(taskRequestDTO.description());
+        taskEntity.setStatus(taskRequestDTO.status());
+        taskEntity.setPriority(taskRequestDTO.priority());
+        taskEntity.setDueDate(taskRequestDTO.dueDate());
+
+        return taskEntity;
+    }
+
+
+}
