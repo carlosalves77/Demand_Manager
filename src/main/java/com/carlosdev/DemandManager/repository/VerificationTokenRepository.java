99ac342713c7acef8f88d4fe83c8ff5c93b6e6ba
Carlos Alves <carlosaalves07@outlook.com>
2025-10-24 21:08:57 -0300

    feat: novo repository para novos usuários

---
+++ src/main/java/com/carlosdev/DemandManager/repository/VerificationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.carlosdev.DemandManager.repository;
+
+import com.carlosdev.DemandManager.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, UUID> {
+
+    Optional<VerificationToken> findByToken(String token);
+}
